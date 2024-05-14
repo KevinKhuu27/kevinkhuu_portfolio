d66b4da062acd015f957a6114c8c4ebd7c0b2991
KevinKhuu27 <khuu.kevin27@hotmail.com>
2024-05-14 16:21:45 -0400

    Added Home section

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-import Testimonials from "./components/Testimonials";
 
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
+      <Home />
       <About />
       <Projects />
       <Skills />
-      <Testimonials />
       <Contact />
     </main>
   );
--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { CodeIcon } from "@heroicons/react/solid";
 
 export default function About() {
   return (
     <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="flex flex-col w-full mb-20">
+          <CodeIcon className="mx-auto inline-block w-10 mb-4" />
+          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+            About Me
+          </h1>
+        </div>
+        <div className="flex flex-wrap -m-4 lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="headshot"
+            src="./headshot2.jpg"
+          />
+        </div>
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hey, I'm Kevin.
@@ -16,13 +30,6 @@ export default function About() {
               Projects
             </a>
           </div>
-        </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          <img
-            className="object-cover object-center rounded"
-            alt="hero"
-            src="./coding.svg"
-          />
         </div>
       </div>
     </section>
