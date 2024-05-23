ff8f054af14086bad79e1ae6107cc34968e0cd71
KevinKhuu27 <khuu.kevin27@hotmail.com>
2024-05-23 14:16:24 -0400

    updated projects and skills section

--- my-portfolio/src/data.js
+++ my-portfolio/src/data.js
@@ -1,9 +1,9 @@
 export const projects = [
   {
     title: "Salsa Club Management App",
-    technology: "HTML, CSS, JavaScript, Python",
+    technology: "HTML, CSS, JavaScript, Python, Firebase",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "An application used to manage a Salsa Club. Incorporates the use of front-end and back-end technology. Includes a user interface for users to interact with. Utlizes Firebase to store and manage data.",
     demo: "https://cps-406-shops--salsa-studio.firebaseapp.com/",
     code: "https://github.com/minhTMUstudent/CPS406_iteration_2_41",
     image: "./project-4.gif",
@@ -12,37 +12,37 @@ export const projects = [
     title: "Sign-up Form",
     technology: "HTML, CSS",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "A sign-up form for users to login to a service. Incorporates UX and responsive web design features. ",
     image: "./project-3.gif",
   },
   {
     title: "Calculator",
     technology: "HTML, CSS",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "A web-based calculator. Built using fundamental HTML and CSS concepts.",
     image: "./project1.gif",
   },
   {
     title: "Rock Paper Scissors",
     technology: "HTML, CSS",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "A Rock Paper Scissors game built using web programming technology.",
     image: "./project2.gif",
   },
   {
-    title: "Text-based Music Simulator",
+    title: "Text-based Music Player",
     technology: "Java",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "A text-based music player that allows users to create playlists, add/remove songs, and display lyrics. Built using Java, applying various concepts. Incorporates OOP, inheritance, and error handling.",
     image: "./project-4.gif",
   },
 ];
 
 export const skills = [
+  "Java",
+  "Python",
+  "HTML",
+  "CSS",
   "JavaScript",
   "React",
-  "Next.js",
-  "Node",
-  "GraphQL",
-  "Material UI",
 ];
